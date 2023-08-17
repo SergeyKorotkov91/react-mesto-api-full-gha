9a7fea3be104a80c5d1211e0a5541558962364a4
Sergey Korotkov <kanc1er@yandex.ru>
2023-08-18 01:39:42 +0300

    auth fix

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.kanc1er.nomoreparties.co";
 //export const BASE_URL = "";
 //export const BASE_URL = 'http://localhost:3000';
 
